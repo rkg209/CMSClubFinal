5d19a35691611fd754ee25ab23405794f9e9f64c
RKG <rahulyk09@gmail.com>
2022-06-30 22:58:44 +0530

    More Done

--- app/src/main/java/com/example/cms/MainActivity.java
+++ app/src/main/java/com/example/cms/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        reference= FirebaseDatabase.getInstance().getReference();
+        reference.keepSynced(true);
     }
 }
---
+++ app/src/main/java/com/example/cms/Offline.java
@@ -0,0 +1,13 @@
+package com.example.cms;
+
+import android.app.Application;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Offline extends Application {
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+    }
+}
