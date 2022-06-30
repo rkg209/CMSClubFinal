72f07f583cecdf0d21f29f243c44d39fd83a17b5
RKG <rahulyk09@gmail.com>
2022-06-30 22:42:57 +0530

    More Done

--- app/src/main/java/com/example/cms/fragment_more.java
+++ app/src/main/java/com/example/cms/fragment_more.java
@@ -1,63 +1,135 @@
 package com.example.cms;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link fragment_more#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.bumptech.glide.Glide;
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class fragment_more extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public CircleImageView clubLogo;
+    public TextView username;
+    public TextView email;
+    public EditText fullname;
+    public EditText thought;
+    public EditText officiallink;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public AppCompatButton logo_button;
+    public Button update;
 
-    public fragment_more() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_more.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static fragment_more newInstance(String param1, String param2) {
-        fragment_more fragment = new fragment_more();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private static String currentUser;
+    private FirebaseStorage storage;
+    private DatabaseReference reference;
+    FirebaseUser user;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false);
+        //return inflater.inflate(R.layout.fragment_more, container, false);
+        View view =  inflater.inflate(R.layout.fragment_more, container, false);
+
+        clubLogo= view.findViewById(R.id.more_profile_pic);
+        username= view.findViewById(R.id.more_uname);
+        email= view.findViewById(R.id.more_email);
+        thought= view.findViewById(R.id.more_thought);
+        fullname= view.findViewById(R.id.more_name);
+        officiallink= view.findViewById(R.id.more_link);
+
+        logo_button=view.findViewById(R.id.more_btn_edit_profile_img);
+        update = view.findViewById(R.id.more_update);
+
+        reference= FirebaseDatabase.getInstance().getReference();
+        storage = FirebaseStorage.getInstance();
+        user= FirebaseAuth.getInstance().getCurrentUser();
+        currentUser = user.getDisplayName();
+
+//        Toast.makeText(getContext(), currentUser, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(getContext(), user.getEmail(), Toast.LENGTH_SHORT).show();
+
+        username.setText(currentUser);
+        email.setText(user.getEmail().toString());
+        FirebaseDatabase.getInstance().getReference().child("Club").child(currentUser).child("Profile").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                thought.setText(snapshot.child("Thought").getValue(String.class));
+                fullname.setText(snapshot.child("Full Name").getValue(String.class));
+                officiallink.setText(snapshot.child("Official Link").getValue(String.class));
+                String uri= snapshot.child("Logo").getValue(String.class);
+                Glide.with(clubLogo.getContext()).load(uri).into(clubLogo);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        logo_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ImagePicker.with(fragment_more.this)
+                        .saveDir(requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES))
+                        .crop()	    			//Crop image(Optional), Check Customization for more option
+                        .compress(1024)			//Final image size will be less than 1 MB(Optional)
+                        .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                        .start();
+            }
+        });
+
+ /*.       @Override
+        protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+
+            Uri uri = data.getData();
+            clubLogo.setImageURI(uri);
+
+            StorageReference MBreference= storage.getReference().child(currentUser +"/logo.jpg");
+            MBreference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    MBreference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            reference.child("Club").child(currentUser).child("Profile").child("Logo").setValue(uri.toString());
+                        }
+                    });
+                }
+            });
+
+
+        }*/
+
+        return view;
+
     }
 }
